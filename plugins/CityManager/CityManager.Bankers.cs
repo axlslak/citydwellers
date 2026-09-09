954023d05e92c55e76f29969d7d16e9cb8bc9153
axlslak <axl@dale.ro>
2026-09-09 21:24:57 +0300

    Mark member withdrawal transaction work

--- plugins/CityManager/CityManager.Bankers.cs
+++ plugins/CityManager/CityManager.Bankers.cs
@@ -10,7 +10,6 @@ using AOSharp.Clientless;
 using AOSharp.Clientless.Logging;
 using CityBankers;
 using CityBankers.Shared;
-using CityDwellers.Shared;
 using Newtonsoft.Json.Linq;
 
 namespace CityManager
